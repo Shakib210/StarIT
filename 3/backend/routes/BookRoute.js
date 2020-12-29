c485ddd3bd1b5cd3753e6e7107779d8c012edee8
Shakib210 <34151336+Shakib210@users.noreply.github.com>
2020-12-29 15:40:30 +0600

    Update BookRoute.js

--- 3/backend/routes/BookRoute.js
+++ 3/backend/routes/BookRoute.js
@@ -4,7 +4,7 @@ import { createBook, deleteBook, getBookByName, updateBook } from '../controller
 import { librarian, protect } from '../middleware/userMiddleware.js'
 
 router.route('/').post(protect,librarian,createBook)
-router.route('/:name').get(getBookByName)
+router.route('/:name').get(protect, getBookByName)
 
 
 router.route('/:id').delete(protect,librarian,deleteBook).put(protect,librarian,updateBook)
